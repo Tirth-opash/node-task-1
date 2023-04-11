398f5be34203f6246fdbe3310bb1180ba99abf3e
Tirth <tirthghelani0701@gmail.com>
2023-04-11 17:44:17 +0530

    add pagination

--- routes/route.js
+++ routes/route.js
@@ -74,20 +74,54 @@ router.get("/search/:key", async (req, res) => {
                 { address: new RegExp(req.params.key, "i") },
             ]
         })
-        console.log(result);
         res.json({ result });
     } catch (error) {
         res.status(500).json(error)
     }
 })
 
-router.get("/sort",async(req,res)=>{
+router.get("/sort",paginatedResult(User),async(req,res)=>{
     try {
-        let result=await User.find().sort({fullname:1})
-        res.json({result})
+        res.json(res.paginatedResult)
     } catch (error) {
         res.status(500).json(error)
     }
 })
+
+function paginatedResult(modal) {
+    return async (req, res, next) => {
+        const page = parseInt(req.query.page);
+        const limit = parseInt(req.query.limit);
+
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
+
+        const results = {}
+
+        if (endIndex < await modal.countDocuments().exec()) {
+            results.next = {
+                page: page + 1,
+                limit: limit
+            }
+        } 
+        if (startIndex > 0) {
+            results.previous = {
+                page: page - 1,
+                limit: limit
+            }
+        }
+        
+        try {
+            results.results = await modal.find().sort({fullname:1}).limit(limit).skip(startIndex).exec();
+            res.paginatedResult = results;
+            next();
+        } catch (e) {
+            res.status(500).json({ message: e.message })
+        }
+    }
+}
+
+
+
 
 module.exports = router;
